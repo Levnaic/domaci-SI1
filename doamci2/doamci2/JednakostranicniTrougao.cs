9cdf8cca0ec284474011a7ec360fa91841b19190
unknown <srdjanlevnaic@gmail.com>
2022-12-09 06:32:44 +0100

    drugi zadatak skoro zavrsen

---
+++ doamci2/doamci2/JednakostranicniTrougao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doamci2
+{
+     class JednakostranicniTrougao : RaznostranicniTrougao
+    {
+        public JednakostranicniTrougao(double aa) : base (aa, aa, aa, aa) { }
+        
+    }
+}
