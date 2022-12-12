cc106c0dfb7cbe1da6706da9d6facfe8fd32dc85
unknown <srdjanlevnaic@gmail.com>
2022-12-12 15:59:00 +0100

    pocetak 3. zadatka

--- doamci2/doamci2/Form1.cs
+++ doamci2/doamci2/Form1.cs
@@ -5,6 +5,50 @@ namespace doamci2
         public Form1()
         {
             InitializeComponent();
+            txtB.Enabled= false;
+            txtC.Enabled= false;
+            txtHa.Enabled= false;
+        }
+
+        private void radiobutton1_checkedchanged(object sender, eventargs e)
+        {
+
+        }
+
+        private void radiobutton1_checkedchanged_1(object sender, eventargs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbRaznostranicni_CheckedChanged(object sender, EventArgs e)
+        {
+            txtB.Enabled= true;
+            txtC.Enabled= true;
+            txtHa.Enabled= true;
+        }
+
+        private void rbJednakostranicni_CheckedChanged(Object sender, EventArgs e) 
+        {
+            txtB.Enabled = false;
+            txtC.Enabled= false;
+            txtHa.Enabled= false;
+        }
+
+        private void rbJednakokraki_CheckedChanged(Object sender, EventArgs e)
+        {
+            txtB.Enabled= true;
+            txtC.Enabled = false;
+            txtHa.Enabled= true; 
         }
     }
 }
