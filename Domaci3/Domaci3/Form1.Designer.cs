9dbe2ff4aa8432059bb2611957b3241384f22fb5
Levnaic <srdjanlevnaic@gmail.com>
2023-02-13 15:48:02 +0100

    zavrsen 3.

--- Domaci3/Domaci3/Form1.Designer.cs
+++ Domaci3/Domaci3/Form1.Designer.cs
@@ -166,6 +166,7 @@
             this.btnDot.TabIndex = 12;
             this.btnDot.Text = ".";
             this.btnDot.UseVisualStyleBackColor = true;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
             // 
             // btnSqrt
             // 
--- Domaci3/Domaci3/Form1.cs
+++ Domaci3/Domaci3/Form1.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Emit;
+
 namespace Domaci3
 {
     public partial class Form1 : Form
@@ -7,7 +9,7 @@ namespace Domaci3
             InitializeComponent();
         }
 
-        private void click_Event(Button btn)
+        private void click_Event_Number(Button btn)
         {
             if (resultIsOn == true)
             {
@@ -17,133 +19,125 @@ namespace Domaci3
             resultIsOn = false;
         }
 
+        private void click_Event_Operand(String operand)
+        {
+            optional = operand;
+            num1 = float.Parse(txtDisplay.Text);
 
-        int num1;
-        int num2;
+            txtDisplay.Text = ("");
+        }
+
+
+        float num1;
+        float num2;
+        float result;
         string optional;
-        int result;
         bool resultIsOn;
 
     
         private void btn1_Click(object sender, EventArgs e)
         {
-            click_Event(btn1);
+            click_Event_Number(btn1);
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            click_Event(btn2);
+            click_Event_Number(btn2);
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            click_Event(btn3);
+            click_Event_Number(btn3);
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            click_Event(btn4);
+            click_Event_Number(btn4);
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            click_Event(btn5);
+            click_Event_Number(btn5);
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            click_Event(btn6);
+            click_Event_Number(btn6);
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            click_Event(btn7);
+            click_Event_Number(btn7);
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            click_Event(btn8);
+            click_Event_Number(btn8);
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            click_Event(btn9);
+            click_Event_Number(btn9);
         }
 
         private void btn0_Click(object sender, EventArgs e)
         {
-            click_Event(btn0);
+            click_Event_Number(btn0);
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            click_Event_Number(btnDot);
         }
 
         private void btnSub_Click(object sender, EventArgs e)
         {
-            optional = "+";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("+");
         }
 
         private void btnMin_Click(object sender, EventArgs e)
         {
-            optional = "-";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("-");
         }
 
         private void btnDiv_Click(object sender, EventArgs e)
         {
-            optional = "/";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("/");
         }
 
         private void btnMul_Click(object sender, EventArgs e)
         {
-            optional = "*";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("*");
         }
 
         private void btnSqrt_Click(object sender, EventArgs e)
         {
-            optional = "sqrt";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("sqrt");
         }
 
         private void btnSqr_Click(object sender, EventArgs e)
         {
-            optional = "sqr";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("sqr");
         }
 
         private void btnR_Click(object sender, EventArgs e)
         {
-            optional = "r";
-            num1 = int.Parse(txtDisplay.Text);
-
-            txtDisplay.Text = ("");
+            click_Event_Operand("r");
         }
 
         private void btnEqu_Click(object sender, EventArgs e)
         {
-            if(optional == "sqrt")
+            if (optional == "sqrt")
             {
-                result = (int)Math.Sqrt(num1);
-            } 
-            else if(optional == "r")
+                result = (float)Math.Sqrt(num1);
+            }
+            else if (optional == "r")
             {
                 result = 1 / num1;
             }
             else
             {
-                num2 = int.Parse(txtDisplay.Text);
+                num2 = float.Parse(txtDisplay.Text);
             }
 
             switch (optional)
@@ -161,7 +155,7 @@ namespace Domaci3
                     result = num1 / num2;
                     break;
                 case "sqr":
-                    result = (int)Math.Pow(num1, num2);
+                    result = (float)Math.Pow(num1, num2);
                     break;
                 default:
                     txtDisplay.Text = "Niste dobro uneli vrednosti";
@@ -171,5 +165,7 @@ namespace Domaci3
             resultIsOn = true;
             txtDisplay.Text = (result.ToString());
         }
+
+      
     }
 }
