27c1bcdf623f639c2655c3ed1dc410a3d1fdb3d2
Levnaic <srdjanlevnaic@gmail.com>
2023-05-15 20:59:24 +0200

    zavrsena forma na 4.

--- domaci4/domaci4/Form1.Designer.cs
+++ domaci4/domaci4/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtZvanje = new System.Windows.Forms.TextBox();
+            this.txtKatedra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.dgProfesori = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProfesori)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(142, 56);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(171, 23);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(142, 85);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(171, 23);
+            this.txtPrezime.TabIndex = 1;
+            // 
+            // txtZvanje
+            // 
+            this.txtZvanje.Location = new System.Drawing.Point(142, 114);
+            this.txtZvanje.Name = "txtZvanje";
+            this.txtZvanje.Size = new System.Drawing.Size(171, 23);
+            this.txtZvanje.TabIndex = 2;
+            // 
+            // txtKatedra
+            // 
+            this.txtKatedra.Location = new System.Drawing.Point(142, 143);
+            this.txtKatedra.Name = "txtKatedra";
+            this.txtKatedra.Size = new System.Drawing.Size(171, 23);
+            this.txtKatedra.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime Profesora:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Prezime Profesora:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Zvanje Profesora:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Katedra Profesora:";
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(38, 194);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(63, 30);
+            this.btnDodaj.TabIndex = 8;
+            this.btnDodaj.Text = "Unos";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // dgProfesori
+            // 
+            this.dgProfesori.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProfesori.Location = new System.Drawing.Point(12, 271);
+            this.dgProfesori.Name = "dgProfesori";
+            this.dgProfesori.RowTemplate.Height = 25;
+            this.dgProfesori.Size = new System.Drawing.Size(338, 167);
+            this.dgProfesori.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(362, 450);
+            this.Controls.Add(this.dgProfesori);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKatedra);
+            this.Controls.Add(this.txtZvanje);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProfesori)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtIme;
+        private TextBox txtPrezime;
+        private TextBox txtZvanje;
+        private TextBox txtKatedra;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnDodaj;
+        private DataGridView dgProfesori;
     }
 }
