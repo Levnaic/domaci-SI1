7451f77a24585e196ae44fc2c3cb4f92a2f87618
Levnaic <srdjanlevnaic@gmail.com>
2023-05-17 00:06:48 +0200

    pre buksne

--- domaci6/domaci6/Form1.cs
+++ domaci6/domaci6/Form1.cs
@@ -2,9 +2,21 @@ namespace domaci6
 {
     public partial class Form1 : Form
     {
+        List<Predmet> predmetList = new List<Predmet>();
+        string akcija = "";
+        int indeksSelektovanog = -1;
+
         public Form1()
         {
             InitializeComponent();
+            
+            dgPredmeti.AllowUserToAddRows= false;
+            dgPredmeti.AllowUserToDeleteRows= false;
+            dgPredmeti.ReadOnly = true;
+            dgPredmeti.AutoGenerateColumns= false;
+
+            dgPredmeti.Columns.Add("ID", "ID");
+            dgPredmeti.Columns["ID"].Visible= false;
         }
     }
 }
